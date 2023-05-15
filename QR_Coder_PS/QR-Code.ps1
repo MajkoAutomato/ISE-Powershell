0d913664fc9323bb4466a378a8eefd7f976e6cf9
MajkoAutomato <126296940+MajkoAutomato@users.noreply.github.com>
2023-05-15 20:17:43 +0200

    Add .ps1 QR_Coder

---
+++ QR_Coder_PS/QR-Code.ps1
@@ -0,0 +1,20 @@
+﻿
+
+'How to generate QR-Code using .Net-Framework for Powershell?'
+
+	Install-Module -Name QRCodeGenerator;
+
+'Hit Y and enter to confirm installation of PackageManagement NUGET'
+
+		Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy Unrestricted;
+
+'Hit Y and enter to run script on localmachine'
+
+		New-PSOneQRCodeURI -URI 'https:\\MajkoAutomato.github.io/' -Width 23 -OutPath 'C:\QR-Code.PNG';
+
+
+
+
+'Command New-PSoneQRCodeURI -URI will create  QR Code for website https:\\MajkoAutomato.github.io/ in output file C:\QR-Code.PNG'
+ 
+#Module QRCodeGenerator will show more functions or aliases if needed by using command Get-Command -Module QRCodeGenerator
---
+++ QR_Coder_PS/README.txt
@@ -0,0 +1,15 @@
+How to generate QR-Code using packagebuilder NuGet in .Net library for Powershell ?
+
+
+1. Run Powershell as admin
+
+2. Copy and paste script
+
+3. Follow instruction 
+
+
+
+Overview of QRCodeGenerator
+
+More information about aliases and function can be request by Get-Command -Module QRCodeGenerator
+
