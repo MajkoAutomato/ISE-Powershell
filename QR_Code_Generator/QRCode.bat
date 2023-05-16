9c3eae808cbc2dd8f3c1ccf5bacea847ce4c6fb8
MajkoAutomato <126296940+MajkoAutomato@users.noreply.github.com>
2023-05-16 20:06:17 +0200

    Add new version of QR_Code_Generator

--- QR_Code_Generator/QRCode.bat
+++ QR_Code_Generator/QRCode.bat
@@ -10,15 +10,15 @@ mkdir QR_CODE_WEBSITE
 
 cd QR_CODE_WEBSITE
 
-set /p input= Please type your full website like https:\\www.example.com Type here: 
+set /p input= Please type your full website like https://www.example.com Type here: 
 echo Your Website is: %input% 
-echo %input% >> YourWebSiteName.txt
+echo %input% > YourWebSiteName.txt
 PAUSE
 
 echo Type any key to close the window and check the target folder C:\QR_CODE_WEBSITE
 PAUSE
 
 echo Your QR-Code is stored in file QRwebsite.txt
-curl qrcode.show -d @C:\QR_CODE_WEBSITE\YourWebSiteName.TXT >> QRwebsite.txt
+curl qrcode.show -d @C:\QR_CODE_WEBSITE\YourWebSiteName.TXT > QRwebsite.txt
 
 PAUSE
